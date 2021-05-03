815a6726d79fcdaf7e1570825af2ee77aeeaa1c8
Yomans <ahmedraad220@outlook.com>
2021-05-03 13:28:44 +0200

    added character element to character card using javaScript

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@
 let characterGallery = document.querySelector(".character_gallery");
 
 function createCharacterCard(name) {
+
   let characterCard = document.createElement("div");
   characterCard.classList.add("character_card");
   let galleryImg = document.createElement("div");
@@ -27,10 +28,28 @@ function createCharacterCard(name) {
   );
   characterImg.setAttribute("alt", name);
   let imgAnchor = document.createElement("a");
-  imgAnchor.href="resources.html";
+  imgAnchor.href = "resources.html";
   imgAnchor.appendChild(characterImg);
   galleryImg.append(imgAnchor);
   characterCard.append(galleryImg);
+
+  fetch("https://api.genshin.dev/characters/" + name)
+  .then((res) => res.json())
+  .then((data) => {
+    let vision = data.vision;
+    let characterElement = document.createElement("p");
+    switch(vision) {
+      case "Pyro":
+        characterElement.classList.add("element_pyro");
+        break;
+      case "Geo":
+        characterElement.classList.add("element_geo");
+        break;
+    }
+    characterElement.innerText = "Element: " + vision;
+    characterElement.classList.add("character_name");
+    characterCard.append(characterElement);
+  });
 
   let characterName_a = document.createElement("a");
   let characterName_link = document.createTextNode(fullName);
@@ -39,7 +58,7 @@ function createCharacterCard(name) {
   characterName_a.href = "resources.html";
   let characterName = document.createElement("p");
   characterName.classList.add("character_name");
-  characterName.append(characterName_a)
+  characterName.append(characterName_a);
   characterCard.append(characterName);
   return characterCard;
 }
