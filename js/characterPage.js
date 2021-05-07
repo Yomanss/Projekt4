c20cd9530d68f5488733f19204f7bc6c0e9b7c09
Yomans <ahmedraad220@outlook.com>
2021-05-07 15:13:04 +0200

    began work on adding auto generated html with javascript for characterPage.html

--- js/characterPage.js
+++ js/characterPage.js
@@ -1,6 +1,7 @@
 "use strict";
 
 let factBox = document.querySelector(".fact_box");
+let characterArticle = document.querySelector(".character_article");
 
 const urlVarString = window.location.search;
 console.log(urlVarString);
@@ -8,6 +9,28 @@ const urlVarParam = new URLSearchParams(urlVarString);
 let characterID = urlVarParam.get("character_ID");
 console.log(characterID);
 
+function createCharacterArticle(description) {
+let articleSection = document.createElement("section");
+let sectionH2 = document.createElement("h2");
+sectionH2.innerHTML = "Amber";
+articleSection.append(sectionH2);
+let sectionDescription = document.createElement("p");
+sectionDescription.innerText = description;
+articleSection.append(sectionDescription);
+return articleSection;
+}
+
+function updateArticle() {
+    fetch("https://api.genshin.dev/characters/" + characterID)
+    .then((res) => res.json())
+    .then((data) => {
+        let description = data.description;
+        characterArticle.append(createCharacterArticle(description));
+    });
+}
+
 function createFactBox() {
 
 }
+
+updateArticle();
