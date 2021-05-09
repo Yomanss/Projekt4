1285447823125efb0ea44b392e73bd11f995c80b
Yomans <ahmedraad220@outlook.com>
2021-05-09 23:02:21 +0200

    started work on fact box by adding image to it, replaced .png images to .jpg

--- js/characterPage.js
+++ js/characterPage.js
@@ -7,32 +7,43 @@ const urlVarString = window.location.search;
 console.log(urlVarString);
 const urlVarParam = new URLSearchParams(urlVarString);
 let characterID = urlVarParam.get("character_ID");
+let displayedName = characterID.charAt(0).toUpperCase() + characterID.slice(1);
 console.log(characterID);
 
 function createCharacterArticle(description, talentSkill) {
-let name = characterID.charAt(0).toUpperCase() + characterID.slice(1);
-let articleSection = document.createElement("section");
-let sectionH2 = document.createElement("h2");
-sectionH2.innerHTML = name;
-articleSection.append(sectionH2);
-let sectionDescription = document.createElement("p");
-sectionDescription.innerText = description;
-articleSection.append(sectionDescription);
-console.log(talentSkill);
-return articleSection;
+  let articleSection = document.createElement("section");
+  let sectionH2 = document.createElement("h2");
+  sectionH2.innerHTML = displayedName;
+  articleSection.append(sectionH2);
+  let sectionDescription = document.createElement("p");
+  sectionDescription.innerText = description;
+  articleSection.append(sectionDescription);
+  console.log(talentSkill);
+  return articleSection;
 }
 
 function updateArticle() {
-    fetch("https://api.genshin.dev/characters/" + characterID)
+  fetch("https://api.genshin.dev/characters/" + characterID)
     .then((res) => res.json())
     .then((data) => {
-        let description = data.description;
-        let talentSkill = data.talentSkill;
-        characterArticle.append(createCharacterArticle(description, talentSkill));
+      let description = data.description;
+      let talentSkill = data.talentSkill;
+      characterArticle.append(createCharacterArticle(description, talentSkill));
+      factBox.append(createFactBox());
     });
 }
 
 function createFactBox() {
+    let factboxDiv = document.createElement("div");
+  let factboxImgDiv = document.createElement("div");
+  factboxImgDiv.classList.add("factbox_imgDiv");
+  let factboxImg = document.createElement("img");
+  factboxImg.setAttribute("src", "./images/" + displayedName + ".jpg");
+  factboxImg.setAttribute("alt", displayedName);
+  factboxImg.classList.add("factbox_img");
+  factboxImgDiv.append(factboxImg);
+  factboxDiv.append(factboxImgDiv);
+  return factboxDiv;
 
 }
 
